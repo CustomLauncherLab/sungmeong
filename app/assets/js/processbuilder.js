6f6b58f48324d3d58271d217b0bd7c3554cde946
go-tiger <dev.gotiger@gmail.com>
2024-12-24 21:02:49 +0900

    v2.0.0

--- app/assets/js/processbuilder.js
+++ app/assets/js/processbuilder.js
@@ -419,7 +419,7 @@ class ProcessBuilder {
 
         // Java Arguments
         if(process.platform === 'darwin'){
-            args.push('-Xdock:name=leafiaworld')
+            args.push('-Xdock:name=sungmeong')
             args.push('-Xdock:icon=' + path.join(__dirname, '..', 'images', 'minecraft.icns'))
         }
         args.push('-Xmx' + ConfigManager.getMaxRAM(this.server.rawServer.id))
@@ -521,7 +521,7 @@ class ProcessBuilder {
                             val = args[i].replace(argDiscovery, tempNativePath)
                             break
                         case 'launcher_name':
-                            val = args[i].replace(argDiscovery, 'Helios-Launcher')
+                            val = args[i].replace(argDiscovery, 'sungmeong-Launcher')
                             break
                         case 'launcher_version':
                             val = args[i].replace(argDiscovery, this.launcherVersion)
